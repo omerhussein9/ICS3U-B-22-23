b75fa824896e865fd0c8efb6b234b3b94c01a11d
Omer Hussein <ohussein@bayviewglen.ca>
2022-09-09 11:07:27 -0400

    Initial Commit

---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleOne.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class ExampleOne {
+    public static void main(String[] args) {
+        System.out.println("soccer on bottom");
+        System.out.println("soccer on bottom");
+        System.out.println("soccer on bottom");
+
+        System.out.println();
+
+        System.out.print("A");
+        System.out.print("B");
+        System.out.print("C");
+    }
+}
---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -0,0 +1,9 @@
+package day2;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("Computer Science is the \"Best\" class ever!");
+
+        // they are excape sequences \n, \", \\
+    }
+}
