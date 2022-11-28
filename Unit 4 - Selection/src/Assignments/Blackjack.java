b1517472a671c38250e935b128de9d73e8f299e4
Omer Hussein <ohussein@bayviewglen.ca>
2022-11-28 10:23:14 -0500

    hello

--- Unit 4 - Selection/src/Assignments/Blackjack.java
+++ Unit 4 - Selection/src/Assignments/Blackjack.java
@@ -2,6 +2,8 @@ package Assignments;
 
 import java.util.Scanner;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class Blackjack {
     
     static Scanner in = new Scanner(System.in);
@@ -11,6 +13,7 @@ public class Blackjack {
     static final int LOST = -1;
     static final int TIE = 0;
     static final int NUM_SUITS = 4;
+    static final int BLACK_JACK = 21;
     private static final String HEARTS = "H";
     private static final String SPADES = "S";
     private static final String CLUBS = "C";
@@ -36,29 +39,118 @@ public class Blackjack {
 
             int result = playHand(playerHand, dealerHand);
 
-            if(result == WIN)
+            if(result == WIN) {
                 wallet += bet;
-            else if(result == LOST)
+                System.out.println("Player WINS!");
+            }
+            else if(result == LOST) {
                 wallet -= bet;
+                System.out.println("Player LOSES...");
+            }
+            if(result == TIE)
+                System.out.println("Player and Dealer TIE!");
 
             if(wallet < MIN_BET) {
                 stillPlaying = false;
                 System.out.println("Game Over. Insuffiecent funds.");
             }
-            else   
-                stillPlaying = playAgain();
+            else
+                stillPlaying = playAgain(wallet);
         }
     }
 
-    private static boolean playAgain() {
-        return false;
+    private static boolean playAgain(int wallet) {
+        System.out.println("You have $" + wallet + " left.");
+        while(true) {
+            System.out.print("Would you like to play again? (Y/N): ");
+            String line = in.nextLine();
+            if(line.equals("Y") || line.equals("y") || line.equals("yes") || line.equals("YES"))
+                return true;
+            else if(line.equals("N") || line.equals("n") || line.equals("no") || line.equals("NO"))
+                return false;
+            else {
+                System.out.println("Invalid Response");
+            }
+        }
     }
 
     //return WIN if player wins, LOST if player lost, TIE if player ties
     private static int playHand(String playerHand, String dealerHand) {
-        return 0;
+        playerHand = playerTurn(playerHand);
+        dealerHand = dealerTurn(dealerHand);
+
+        int playerScore = getCardsValue(playerHand);
+        int dealerScore = getCardsValue(dealerHand);
+
+        if(dealerScore <= BLACK_JACK && (dealerScore > playerScore || playerScore > BLACK_JACK))
+            return LOST;
+        if((playerScore > dealerScore && playerScore <= BLACK_JACK) || dealerScore > BLACK_JACK) 
+            return WIN;
+        return TIE;
     }
 
+    private static int getCardsValue(String cards) {
+        int numAces = 0;
+
+        int scoreBeforeAces = 0;
+
+        for(int i = 0; i < cards.length(); i++) {
+            String s = cards.substring(i,i + 1);
+            if("JQK1".indexOf(s) >= 0)
+                scoreBeforeAces += 10;
+            else if("23456789".indexOf(s) >= 0)
+                scoreBeforeAces += Integer.parseInt(s);
+            else if(s.equals("A"))
+                numAces++;
+        }
+
+        if(numAces > 0 && ((scoreBeforeAces + 11 + numAces - 1) <= BLACK_JACK))
+            scoreBeforeAces += 11 + numAces - 1;
+        else   
+            scoreBeforeAces += numAces;
+        
+        return scoreBeforeAces;
+    }
+
+    private static String dealerTurn(String dealerHand) {
+        dealerHand += " " + getCard();
+        displayHand(dealerHand, false, "Dealer Hand: ");
+        while(getCardsValue(dealerHand) < 17) {
+            dealerHand += " " + getCard();
+            displayHand(dealerHand, false, "Dealer Hand: ");
+        }
+
+        return dealerHand;
+    }
+
+    private static String playerTurn(String playerHand) {
+        displayHand(playerHand, false, "Player Hand: ");
+
+        while(true) {
+            if(takeCard()) {
+                playerHand += " " + getCard();
+                displayHand(playerHand, false, "Player Hand: ");
+                if(getCardsValue(playerHand) > BLACK_JACK)
+                    return playerHand;
+            }
+            else 
+                return playerHand;
+        }
+    }
+
+    private static boolean takeCard() {
+        while(true) {
+            System.out.print("Hit (1) or Stand (2): ");
+            String result = in.nextLine().toLowerCase();
+
+            if(result.equals("1"))
+                return true;
+            else if(result.equals("2"))
+                return false;
+            else
+                System.out.print("Invalid Answer");
+        }
+    }
 
     private static void displayHand(String cards, boolean isHidden, String label) {
         String extra = isHidden ? "XX " : "";
